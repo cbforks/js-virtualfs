53b4bbf7bc87facd7bd8c4db2b19bd1e8be56f37
Roger Qiu <roger.qiu@polyhack.io>
2017-07-14 23:59:00 +1000

    shell.nix should not be using src, as that is meant for default.nix and nix-build, and would be affected by large temporary files

--- shell.nix
+++ shell.nix
@@ -1,7 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
   with pkgs;
   stdenv.mkDerivation {
-    name = "memory-fs";
-    src = ./.;
+    name = "js-virtualfs";
     buildInputs = [ python2 nodejs nodePackages.node2nix ];
   }
